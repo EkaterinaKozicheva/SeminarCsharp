b1abc081cabcced3f0c1539ee77adc6bb93fe209
Ekaterina <kozicheva-katya@yandex.ru>
2022-08-28 13:43:46 +0300

    Zadacha 2

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -7,3 +7,22 @@ Console.WriteLine("Введите трёхзначное число: ");
 int num = int.Parse(Console.ReadLine());
 int a = num % 100 / 10;
 Console.WriteLine($"Вторая цифра трёхзначного числа {num} -> {a} ");
+
+//Задача 13. Напишите программу, которая которая выводит третью цифру заданного числа или сообщает,
+//что третьей цифры нет
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+string numOther = Convert.ToString(num);
+
+if(numOther.Length > 2) {
+    Console.WriteLine($"Третья цифра: {numOther[2]} ");
+}
+else {
+
+    Console.WriteLine($"Третьей цифры нет ");
+}
