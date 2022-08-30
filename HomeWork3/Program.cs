6b374864ab252d77fe10a5273077b583a53d20b3
Ekaterina <kozicheva-katya@yandex.ru>
2022-08-30 14:14:49 +0300

    Zadacha1

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -1,1 +1,27 @@
-﻿// Задача 19.
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// void Mirror(int num)
+// {
+// int a = num / 10000;
+// int b = (num % 10000) / 1000;
+// int c = (num % 100) / 10;
+// int d = num % 10;
+
+// if(a == d && b == c)
+//     {
+//         Console.WriteLine("Палиндром");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Не палиндром");
+//     }
+// }
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if(number > 99999 || number < 10000) Console.WriteLine("Число не подходит по условию");
+// else Mirror(number);
+
