8182d2e136a4c8e4100cc47ea2eebacb43ce68b7
Ekaterina <kozicheva-katya@yandex.ru>
2022-09-01 20:26:52 +0300

    zadacha2

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -2,15 +2,31 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+// Console.WriteLine("Введите число a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите числоЫ b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int res = a;
 
-Console.WriteLine("Введите число a: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите числоЫ b: ");
-int b = Convert.ToInt32(Console.ReadLine());
-int res = a;
+// for (int i = 1; i < b; i++)
+// {
+//     res = res*a;
+// }
+// Console.WriteLine("A в степени B равно: " + res);
 
-for (int i = 1; i < b; i++)
-{
-    res = res*a;
-}
-Console.WriteLine("A в степени B равно: " + res);
+//Задача 27. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Console.WriteLine("Введите число: ");
+// int i = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+
+// while(i > 0)
+// {
+//     int num  = i % 10;
+//     i = i / 10;
+//     sum = sum + num;
+// }
+// Console.WriteLine(" Сумма всех цифр в числе:"  + sum);
