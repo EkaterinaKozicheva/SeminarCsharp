f9ad3e5da2280fcf03257fbf5a83acfc8975199f
Ekaterina <kozicheva-katya@yandex.ru>
2022-09-07 12:58:30 +0300

    zadacha2

--- HomeWork5/Program.cs
+++ HomeWork5/Program.cs
@@ -2,43 +2,42 @@
 //Напишите программу, которая покажет количество чётных чисел в массиве.
 //[345, 897, 568, 234] -> 2
 
-int[] RandomArray(int a)
- {
-    int[] array = new int[a];         
-        for (int i = 0; i < a; i++)
-        {    
-         array[i] = new Random().Next(100, 1000);
-        }
-    
-    return array;   
-    }
+// int[] RandomArray(int a)
+// {
+//     int[] array = new int[a];
+//     for (int i = 0; i < a; i++)
+//     {
+//         array[i] = new Random().Next(100, 1000);
+//     }
+//     return array;
+// }
 
-void GetArray(int[] array) 
-{  
-    Console.Write($"[ ");
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i]+", ");       
-    }
-     Console.Write($"]");
-}
-void Count(int[] array) 
-{
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] % 2 == 0)   
-        {
-         count ++;
-        }
-    }
-Console.WriteLine($"Количество четных чисел: {count}");    
-}    
-int a = new Random().Next(1, 20);  
-int[] array = RandomArray(a); 
-GetArray(array);
-Console.WriteLine();
-Count(array);
+// void GetArray(int[] array)
+// {
+//     Console.Write($"[ ");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + ", ");
+//     }
+//     Console.Write($"]");
+// }
+// void Count(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] % 2 == 0)
+//         {
+//             count++;
+//         }
+//     }
+//     Console.WriteLine($"Количество четных чисел: {count}");
+// }
+// int a = new Random().Next(1, 20);
+// int[] array = RandomArray(a);
+// GetArray(array);
+// Console.WriteLine();
+// Count(array);
 
 
 
@@ -47,17 +46,42 @@ Count(array);
 //[-4, -6, 89, 6] -> 0
 
 
+int[] RandomArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+    }
+    return array;
+}
 
-
-
-
-
-
-
-
-
-
-
+void GetArray(int[] array)
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write($"]");
+}
+void PrintArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum = array[i] + sum;
+        }
+    }
+    Console.WriteLine($"Сумма чисел на нечетных позициях: {sum} ");
+}
+int size = new Random().Next(1, 20);
+int[] array = RandomArray(size);
+GetArray(array);
+Console.WriteLine();
+PrintArray(array);
 
 
 
