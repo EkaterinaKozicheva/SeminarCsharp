9808734ffe4eb8e6ef4761e59271634f7620a24c
Ekaterina <kozicheva-katya@yandex.ru>
2022-09-14 12:10:35 +0300

    zadacha2

--- HomeWork7/Program.cs
+++ HomeWork7/Program.cs
@@ -5,33 +5,35 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-double[,] GetArray(int rows, int column)
-{
-    double[,] array = new double[rows, column];
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < column; j++)
-            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
-    return array;
-}
+// double[,] GetArray(int rows, int column)
+// {
+//     double[,] array = new double[rows, column];
+//     for (int i = 0; i < rows; i++)
+//         for (int j = 0; j < column; j++)
+//             array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+//     return array;
+// }
 
-void ShowArray(double[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-}
+// void ShowArray(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
-Console.Write("Введите число m: ");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число n: ");
-int cols = Convert.ToInt32(Console.ReadLine());
-double[,] array = GetArray(rows, cols);
-ShowArray(array);
+// Console.Write("Введите число m: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число n: ");
+// int cols = Convert.ToInt32(Console.ReadLine());
+// double[,] array = GetArray(rows, cols);
+// ShowArray(array);
+
+
 
 // =============Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
 //и возвращает значение этого элемента или же указание, что такого элемента нет.
@@ -42,49 +44,48 @@ ShowArray(array);
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
+Console.WriteLine("Введите номер строки: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[10, 10];
+RandomNumbers(numbers);
 
-// Console.WriteLine("введите номер строки");
-// int n = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("введите номер столбца");
-// int m = Convert.ToInt32(Console.ReadLine());
-// int[,] numbers = new int[10, 10];
-// FillArrayRandomNumbers(numbers);
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("Такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"Значение элемента {n} строки и {m} столбца равно {numbers[n - 1, m - 1]}");
+}
 
-// if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
-// {
-//     Console.WriteLine("такого элемента нет");
-// }
-// else
-// {
-//     Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n - 1, m - 1]}");
-// }
+PrintArray(numbers);
 
-// PrintArray(numbers);
+void RandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100) / 10;
+        }
+    }
+}
 
-// void FillArrayRandomNumbers(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             array[i, j] = new Random().Next(-100, 100) / 10;
-//         }
-//     }
-// }
-
-// void PrintArray(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         Console.Write("[ ");
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             Console.Write(array[i, j] + " ");
-//         }
-//         Console.Write("]");
-//         Console.WriteLine("");
-//     }
-// }
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+         Console.WriteLine("");
+    }
+}
 
 
 //===============Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
